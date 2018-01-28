c837a14955839ea010bc4ed845dacd2fa2aa37ee
Linus <linus@ronaldjacob.de>
2018-01-28 18:42:34 +0100

    Some experimenting with jquery and ajax

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var obj1 = {
-    name: "obj1"
-},
-obj2 = {
-    name: "obj2"
-};
+const url = "http://jsonplaceholder.typicode.com/posts/1",
+failUrl = "fail";
+let buttons = document.getElementsByClassName("execButton");
 
-function printName () {
-    return this.name;
+fetch(url)
+.then( function (response) { return response.json() })
+.then( function (responsejson) { console.log(responsejson.id); })
+.catch( function (e) {console.log(e) });
+
+function print(text) {
+    let $button = $(this),
+    $paragraph = $button.next();
+    $paragraph.text(text);
+};
+function printError (err) {
+    let self = $(this);
+    return print ("Error: /n" + err, self);
 }
+function getData () {
+    return new Promise ( function (resolve,reject){
+        $.get( {
+            url: url,
+            error: printError(),
+            success: printSuccess(data)
+        } );
+    })
+}
+
